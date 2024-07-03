3bd141ade2b7b605556b6e5ecbdfa4391f73e2aa
EmmaHenault <161342251+EmmaHenault@users.noreply.github.com>
2024-07-03 18:15:38 +0200

    quete 4

--- src/main/java/com/wcs/blog4/controller/ArticleController.java
+++ src/main/java/com/wcs/blog4/controller/ArticleController.java
@@ -35,6 +35,43 @@ public class ArticleController {
         return ResponseEntity.ok(article);
     }
 
+    @GetMapping("/title/{title}")
+    public ResponseEntity<List<Article>> getArticlesByTitle(@PathVariable String title) {
+        List<Article> articles = articleRepository.findByTitle(title);
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    @GetMapping("/latest")
+    public ResponseEntity<List<Article>> getLatestArticles() {
+        List<Article> articles = articleRepository.findLatest5Articles();
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Article>> getArticlesByContent(@RequestParam String keyword) {
+        List<Article> articles = articleRepository.findByContentContaining(keyword);
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    @GetMapping("/date/{date}")
+    public ResponseEntity<List<Article>> getArticlesByDate(@PathVariable String date) {
+        LocalDateTime createdAt = LocalDateTime.parse(date);
+        List<Article> articles = articleRepository.findByCreatedAt(createdAt);
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
     @PostMapping
     public ResponseEntity<Article> createArticle(@RequestBody Article article) {
         article.setCreatedAt(LocalDateTime.now());
