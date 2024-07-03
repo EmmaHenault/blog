ecc64f7bce8789c4ecc227b94be9c16211518885
Emma Henault <emmahenault@MBP-de-Emma.lan>
2024-07-03 17:13:31 +0200

    first commit

---
+++ src/test/java/com/wcs/blog4/Blog4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.blog4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
